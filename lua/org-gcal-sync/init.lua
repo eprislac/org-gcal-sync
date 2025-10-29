6e2559435b67030e210f68cb3c33b61cfca594ab
Eddie Prislac <11530791+eprislac@users.noreply.github.com>
2025-10-29 00:10:14 -0500

    Adds logging for debugging

--- lua/org-gcal-sync/init.lua
+++ lua/org-gcal-sync/init.lua
@@ -14,18 +14,23 @@ M.export_org = M.sync
 
 function M.setup(opts)
   M.config = vim.tbl_deep_extend("force", M.config, opts or {})
-
+  vim.notify(vim.inspect(myTable), vim.log.levels.INFO)
   -- Create directory
   vim.fn.mkdir(M.config.agenda_dir, "p")
+  vim.notify("table created", vim.log.levels.INFO)
 
   -- Register commands
   vim.api.nvim_create_user_command("SyncOrgGcal", M.sync, { desc = "Sync org ↔ gcal" })
+  vim.notify("SyncOrgGcal added to commands", vim.log.levels.INFO)
   vim.api.nvim_create_user_command("ImportGcal", M.import_gcal, { desc = "Import from gcal" })
+  vim.notify("ImportGcal added to commands", vim.log.levels.INFO)
   vim.api.nvim_create_user_command("ExportOrg", M.export_org, { desc = "Export to gcal" })
-
+  vim.notify("ExportOrg added to commands", vim.log.levels.INFO)
   -- Add to org-agenda
   vim.g.org_agenda_files = vim.g.org_agenda_files or {}
+  vim.notify(vim.inspect(vim.g.org_agenda_files), vim.log.levels.INFO)
   local agenda_path = vim.fn.expand(M.config.agenda_dir) .. "/**/*.org"
+  vim.notify(agenda_path, vim.log.levels.INFO)
   if not vim.tbl_contains(vim.g.org_agenda_files, agenda_path) then
     table.insert(vim.g.org_agenda_files, agenda_path)
   end
@@ -38,7 +43,8 @@ function M.setup(opts)
     M.import_gcal = utils.import_gcal
     M.export_org = utils.export_org
   else
-    vim.notify("org-gcal-sync: utils failed to load: " .. utils, vim.log.levels.ERROR)
+    vim.notify("org-gcal-sync: utils failed to load: " .. u:w
+    tils, vim.log.levels.ERROR)
   end
 end
 
