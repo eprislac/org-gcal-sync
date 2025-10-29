fdd50113c3a96507d64cb8944ddbd8ed09e0023c
Eddie Prislac <11530791+eprislac@users.noreply.github.com>
2025-10-28 23:36:10 -0500

    Add chek for gcalcli

--- lua/org-gcal-sync/health.lua
+++ lua/org-gcal-sync/health.lua
@@ -1,9 +1,23 @@
 -- lua/health.lua
 local M = {}
-
+local health = require('vim.health')
 function M.check()
-  local sync = require("org-gcal-sync")
-  sync.checkhealth()
+  health.start('OrgGcalSync')
+  local gcalcli_exists = vim.fn.executable('gcalcli') == 1
+  if gcalcli_exists then
+    health.ok('gcalcli is installed and executable')
+  else
+    health.error(
+      'gcalcli was not found in your path',
+      {
+	'OrgGcalSync requires gcalcli to function correctly.',
+	'Please ensure it is installed and available in your system path'
+      }
+    )
+  end
+ 
+  -- local sync = require("org-gcal-sync")
+  -- sync.checkhealth()
 end
 
 return M
